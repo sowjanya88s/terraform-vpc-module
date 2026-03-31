93e03fd8c418ea3cf7885a9c53ad470fe5081587
sowjanya <devopssowjanya88s@gmail.com>
2026-03-31 22:04:21 +0530

    new

--- main.tf
+++ main.tf
@@ -10,15 +10,48 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_subnet" "public" {
-    count = length(var.cidr_block)
+    count = length(var.public_cidr_block)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.cidr_block[count.index]
+  cidr_block = var.public_cidr_block[count.index]
   availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
   tags = merge(
     local.common_tags ,
     {
     #roboshop-dev-public-us-east-1
     Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+  },
+  var.subnet_tags
+  )
+}
+
+resource "aws_subnet" "private" {
+    count = length(var.private_cidr_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_cidr_block[count.index]
+  availability_zone = local.az_names[count.index]
+  
+  tags = merge(
+    local.common_tags ,
+    {
+    #roboshop-dev-private-us-east-1
+    Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+  },
+  var.subnet_tags
+  )
+}
+
+resource "aws_subnet" "private" {
+    count = length(var.database_cidr_block)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.database_cidr_block[count.index]
+  availability_zone = local.az_names[count.index]
+  
+  tags = merge(
+    local.common_tags ,
+    {
+    #roboshop-dev-database-us-east-1
+    Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
   },
   var.subnet_tags
   )
--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "igw_tags" {
     default = {}
 }
 
-variable "cidr_block" {
+variable "public_cidr_block" {
     type = list
     default = ["10.0.1.0/24" , "10.0.2.0/24"]
 }
@@ -29,4 +29,14 @@ variable "cidr_block" {
 variable "subnet_tags" {
     type = map
     default = {}
+}
+
+variable "private_cidr_block" {
+    type = list
+    default = ["10.0.11.0/24" , "10.0.12.0/24"]
+}
+
+variable "database_cidr_block" {
+    type = list
+    default = ["10.0.21.0/24" , "10.0.22.0/24"]
 }
