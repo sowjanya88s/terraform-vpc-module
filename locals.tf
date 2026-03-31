90685ad9a60239a06a93ffe0a8a3d838ad8b769a
sowjanya <devopssowjanya88s@gmail.com>
2026-03-31 20:41:03 +0530

    new

--- locals.tf
+++ locals.tf
@@ -2,12 +2,12 @@ locals {
     vpc_final_tags = merge(local.common_tags , var.vpc_tags)
     common_tags = {
         terraform = "true"
-        Name = "${var.project}-${var.environment}
+        Name = "${var.project}-${var.environment}"
     }
   igw_final_tags = merge(local.common_tags , var.igw_tags)
   common_tags = {
         terraform = "true"
-        Name = "${var.project}-${var.environment}
+        Name = "${var.project}-${var.environment}"
     }
 
 }
